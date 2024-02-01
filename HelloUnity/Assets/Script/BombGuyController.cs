0b9a298ab7b1ba4c06e3e83c6f3dfcc47d8ca9ac
Academy07 <rlarudals_96@naver.com>
2024-02-01 19:03:18 +0900

    Pirate Bomb 프로젝트 ★코루틴배운 중요한날★ 점프를 구현해보려고함

---
+++ HelloUnity/Assets/Script/BombGuyController.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using UnityEngine;
+
+enum State
+{
+    Idle, Run, Jump
+}
+
+public class BombGuyController : MonoBehaviour
+{
+    //BombGuyController가 Animator 컴포넌트를 알아야한다.
+    //Animator컴포넌트는 자식 오브젝트 anim에 붙어 있다
+    //어떻게 하면 자식오브젝트에 붙어 있는 Animator컴포넌트를 가져올수 있을까?
+    [SerializeField] Animator anim;
+    [SerializeField] Transform flagTransform;
+
+    private Coroutine coroutine;
+    private int isJumpUp = 0;
+    private int isJumpDown = 2;
+
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+    void Start()
+    {
+        Debug.Log("Start");
+        //Transform animTransform = this.transform.Find("anim");
+        //GameObject animGo = animTransform.gameObject;
+        //this.anim = animGo.GetComponent<Animator>();
+        //Debug.LogFormat("coroutine : {0}", this.coroutine);
+        //코루틴 함수 호출시
+        //this.coroutine = this.StartCoroutine(this.CoMove(() =>
+        //{
+        //    Debug.Log("이동을 완료하였습니다.");
+        //}));
+
+    }
+    IEnumerator CoMove(System.Action callback)
+    {
+        //매 프레임마다 앞으로 이동
+        while (true)
+        {
+            this.transform.Translate(transform.right * 1f * Time.deltaTime);
+            float length = this.flagTransform.position.x - this.transform.position.x;
+            Debug.LogFormat("이동중.. 남은거리 : {0}", length);
+            if (length < 1)
+            {
+                break;
+            }
+            yield return null; //다음프레임으로 넘어간다.
+        }
+        Debug.Log("이동완료");
+        callback();
+    }
+
+    IEnumerator CoJump(System.Action callback)
+    {
+        while (true)
+        {
+            float jump = 0f;
+            if (this.transform.position.y == 2f)
+            {
+                jump = -2f;
+            }
+            if (this.transform.position.y == 0f)
+            {
+                jump = 2f;
+            }
+            this.transform.Translate(transform.up * jump *  Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    //코루틴 멈추기
+        //    StopCoroutine(this.coroutine);
+        //}
+
+        Debug.Log("Update");
+        this.Move();
+    }
+
+    private void Move()
+    {
+        //애니메이션 전환 하기
+        //전화 할때 파라미터의 값을 변경하기
+        Vector3 pos = this.transform.position;
+        float move = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left");
+            move = -0.1f;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right");
+            move = 0.1f;
+        }
+
+        //if (this.isJumpUp == 1 || this.isJumpUp == 2)
+        //{
+        //    pos.y += 1f;
+        //    this.isJumpUp += 1;
+        //    this.isJumpDown -= 1;
+        //}
+        //else if (this.isJumpDown == 0 || this.isJumpDown == 1)
+        //{
+        //    pos.y -= 1f;
+        //    this.isJumpDown += 1;
+        //    this.isJumpUp -= 1;
+        //}
+        //else if (this.isJumpUp == 0)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        pos.y += 1f;
+        //        anim.SetInteger("State", (int)State.Jump);
+        //        this.isJumpUp = +1;
+        //    }
+        //}
+        pos.x += move;
+        this.transform.Translate(pos * Time.deltaTime);
+        float clampX = Mathf.Clamp(pos.x, -8f, 8f);
+        pos.x = clampX;
+        this.transform.position = pos;
+
+        if (move == 0f)
+        {
+            this.anim.SetInteger("State", (int)State.Idle);
+        }
+        else if (Mathf.Abs(move) == 0.1f)
+        {
+            this.anim.SetInteger("State", (int)State.Run);
+        }
+
+    }
+}
